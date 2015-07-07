6b706f7595c93b00c539c8a39cac602b42bdcdaf
Nagibator28 <phelixonik@mail.ru>
2015-07-07 14:58:43 +0600

    sine and a module for a single argument

--- Calculator/MainForm.Designer.cs
+++ Calculator/MainForm.Designer.cs
@@ -35,13 +35,15 @@
             this.Result = new System.Windows.Forms.TextBox();
             this.SecondArgument = new System.Windows.Forms.TextBox();
             this.FirstArgument = new System.Windows.Forms.TextBox();
+            this.Sinus = new System.Windows.Forms.Button();
+            this.Module = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Addition
             // 
             this.Addition.Location = new System.Drawing.Point(12, 60);
             this.Addition.Name = "Addition";
-            this.Addition.Size = new System.Drawing.Size(152, 37);
+            this.Addition.Size = new System.Drawing.Size(60, 36);
             this.Addition.TabIndex = 0;
             this.Addition.Text = "+";
             this.Addition.UseVisualStyleBackColor = true;
@@ -49,9 +51,9 @@
             // 
             // Subtraction
             // 
-            this.Subtraction.Location = new System.Drawing.Point(170, 60);
+            this.Subtraction.Location = new System.Drawing.Point(78, 60);
             this.Subtraction.Name = "Subtraction";
-            this.Subtraction.Size = new System.Drawing.Size(152, 37);
+            this.Subtraction.Size = new System.Drawing.Size(60, 37);
             this.Subtraction.TabIndex = 1;
             this.Subtraction.Text = "-";
             this.Subtraction.UseVisualStyleBackColor = true;
@@ -59,9 +61,9 @@
             // 
             // Multiplication
             // 
-            this.Multiplication.Location = new System.Drawing.Point(12, 103);
+            this.Multiplication.Location = new System.Drawing.Point(12, 102);
             this.Multiplication.Name = "Multiplication";
-            this.Multiplication.Size = new System.Drawing.Size(152, 37);
+            this.Multiplication.Size = new System.Drawing.Size(60, 37);
             this.Multiplication.TabIndex = 2;
             this.Multiplication.Text = "*";
             this.Multiplication.UseVisualStyleBackColor = true;
@@ -69,9 +71,9 @@
             // 
             // Division
             // 
-            this.Division.Location = new System.Drawing.Point(170, 103);
+            this.Division.Location = new System.Drawing.Point(144, 60);
             this.Division.Name = "Division";
-            this.Division.Size = new System.Drawing.Size(152, 37);
+            this.Division.Size = new System.Drawing.Size(60, 37);
             this.Division.TabIndex = 3;
             this.Division.Text = "/";
             this.Division.UseVisualStyleBackColor = true;
@@ -101,11 +103,33 @@
             this.FirstArgument.Size = new System.Drawing.Size(152, 41);
             this.FirstArgument.TabIndex = 6;
             // 
+            // Sinus
+            // 
+            this.Sinus.Location = new System.Drawing.Point(78, 102);
+            this.Sinus.Name = "Sinus";
+            this.Sinus.Size = new System.Drawing.Size(60, 36);
+            this.Sinus.TabIndex = 7;
+            this.Sinus.Text = "Sin";
+            this.Sinus.UseVisualStyleBackColor = true;
+            this.Sinus.Click += new System.EventHandler(this.Calculate);
+            // 
+            // Module
+            // 
+            this.Module.Location = new System.Drawing.Point(144, 102);
+            this.Module.Name = "Module";
+            this.Module.Size = new System.Drawing.Size(60, 36);
+            this.Module.TabIndex = 8;
+            this.Module.Text = "|Module|";
+            this.Module.UseVisualStyleBackColor = true;
+            this.Module.Click += new System.EventHandler(this.Calculate);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(430, 153);
+            this.Controls.Add(this.Module);
+            this.Controls.Add(this.Sinus);
             this.Controls.Add(this.FirstArgument);
             this.Controls.Add(this.SecondArgument);
             this.Controls.Add(this.Result);
@@ -113,12 +137,12 @@
             this.Controls.Add(this.Multiplication);
             this.Controls.Add(this.Subtraction);
             this.Controls.Add(this.Addition);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "Calculator";
             this.ResumeLayout(false);
             this.PerformLayout();
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.MaximizeBox = false;
 
         }
 
@@ -131,6 +155,8 @@
         private System.Windows.Forms.TextBox Result;
         private System.Windows.Forms.TextBox SecondArgument;
         private System.Windows.Forms.TextBox FirstArgument;
+        private System.Windows.Forms.Button Sinus;
+        private System.Windows.Forms.Button Module;
     }
 }
 
--- Calculator/MainForm.cs
+++ Calculator/MainForm.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Calculator
@@ -21,14 +14,14 @@ namespace Calculator
             return double.Parse(text);
 
         }
-
+        
         private void Calculate(object sender, EventArgs e)
         {
             double firstArgument = StringToDouble(FirstArgument.Text);
             double secondArgument = StringToDouble(SecondArgument.Text);
 
             double result;
-            switch (((Button)sender).Name)
+            switch (((Button) sender).Name)
             {
                 case "Addition":
                     result = firstArgument + secondArgument;
@@ -47,7 +40,6 @@ namespace Calculator
                     {
                         throw new Exception("Devision by zero");
                     }
-
                     result = firstArgument/secondArgument;
                     break;
 
@@ -58,5 +50,27 @@ namespace Calculator
             Result.Text = Convert.ToString(result);
 
         }
-    }
+        
+        private void CalculateSingleArgument(object sender, EventArgs e)
+        {
+            double firstArgument = StringToDouble(FirstArgument.Text);
+
+            double result;
+            switch (((Button)sender).Name)
+            {
+                case "Sinus":
+                    result = Math.Sin(firstArgument);
+                    break;
+
+                case "Module":
+                    result = Math.Abs(firstArgument);
+                    break;
+
+                default:
+                    throw new Exception("Unknown operation");
+            }
+
+            Result.Text = Convert.ToString(result);
+        }
+    }            
 }
