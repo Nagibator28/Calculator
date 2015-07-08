9dd09ea286fb738041bff3d344fba72b8dee77ad
Nagibator28 <phelixonik@mail.ru>
2015-07-08 13:54:19 +0600

    Test Sinus

---
+++ Calculator.Tests/OneArgument/SinusTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using NUnit.Framework;
+using Calculator.OneArgument;
+
+namespace Calculator.Tests.OneArgument
+{
+    [TestFixture]
+    public class SinusTests
+    {
+        [TestCase(0, 0)]
+        public void Calculate(double argument, double result)
+        {
+            var calculator = new Sinus();
+            var testresult = calculator.Calculate(argument);
+            Assert.AreEqual(result, testresult);
+
+        }
+    }
+}
