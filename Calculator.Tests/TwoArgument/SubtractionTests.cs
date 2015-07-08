cc2ccf5397a534c2d7ecfdad2eee6ec04205bb56
Nagibator28 <phelixonik@mail.ru>
2015-07-08 14:11:41 +0600

    Test Subtraction

---
+++ Calculator.Tests/TwoArgument/SubtractionTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using NUnit.Framework;
+using Calculator.TwoArgument;
+
+namespace Calculator.Tests.TwoArgument
+{
+    [TestFixture]
+    public class SubtractionTests
+    {
+        [TestCase(2, 2, 0)]
+        [TestCase(3, 6, -3)]
+        [TestCase(3, -6, 9)]
+        public void Calculate(double firstArgument, double secondArgument, double result)
+        {
+            var calculator = new Subtraction();
+            var testresult = calculator.Calculate(firstArgument, secondArgument);
+            Assert.AreEqual(result, testresult);
+
+        }
+    }
+}
