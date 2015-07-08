32488cb759f98e7c9e76391ff492c6375f033b1b
Nagibator28 <phelixonik@mail.ru>
2015-07-08 14:14:48 +0600

    Test Division

---
+++ Calculator.Tests/TwoArgument/Division.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using NUnit.Framework;
+using Calculator.TwoArgument;
+
+namespace Calculator.Tests.TwoArgument
+{
+    [TestFixture]
+    public class DivisionTests
+    {
+        [TestCase(2, 2, 1)]
+        [TestCase(3, 6, 0.5)]
+        public void Calculate(double firstArgument, double secondArgument, double result)
+        {
+            var calculator = new Division();
+            var testresult = calculator.Calculate(firstArgument, secondArgument);
+            Assert.AreEqual(result, testresult);
+
+        }
+    }
+}
