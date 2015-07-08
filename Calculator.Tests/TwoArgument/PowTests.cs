cbaa801bfa6bac15cc66e091c06a48a0fb6f7549
Nagibator28 <phelixonik@mail.ru>
2015-07-08 14:19:01 +0600

    Test Pow

---
+++ Calculator.Tests/TwoArgument/PowTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using NUnit.Framework;
+using Calculator.TwoArgument;
+
+namespace Calculator.Tests.TwoArgument
+{
+    [TestFixture]
+    public class PowTests
+    {
+        [TestCase(2, 2, 4)]
+        [TestCase(3, 3, 27)]
+        public void Calculate(double firstArgument, double secondArgument, double result)
+        {
+            var calculator = new Pow();
+            var testresult = calculator.Calculate(firstArgument, secondArgument);
+            Assert.AreEqual(result, testresult);
+
+        }
+    }
+}
