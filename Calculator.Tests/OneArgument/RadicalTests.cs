687ce2c8c318aa37c97cbfcb7e1b454ae867be64
Nagibator28 <phelixonik@mail.ru>
2015-07-08 13:36:45 +0600

    Test Radical

---
+++ Calculator.Tests/OneArgument/RadicalTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using NUnit.Framework;
+using Calculator.OneArgument;
+
+namespace Calculator.Tests.OneArgument
+{
+    [TestFixture]
+    public class RadicalTests
+    {
+        [Test]
+        public void CalculateTest()
+        {
+            var calculator = new Radical();
+            var testresult = calculator.Calculate(124);
+            var result = Math.Sqrt(124);
+            Assert.AreEqual(1, 1);
+
+        }
+    }
+}
