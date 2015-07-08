9d26baefd169a4df5e7cdfde4df9e7eb6ed3dabd
Nagibator28 <phelixonik@mail.ru>
2015-07-08 13:51:43 +0600

    Test Module

---
+++ Calculator.Tests/ModuleTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using NUnit.Framework;
+using Calculator.OneArgument;
+
+namespace Calculator.Tests.OneArgument
+{
+    [TestFixture]
+    public class ModuleTests
+    {
+        [TestCase(-24,24)]
+        public void Calculate(double argument, double result)
+        {
+            var calculator = new Module();
+            var testresult = calculator.Calculate(argument);
+            Assert.AreEqual(result, testresult);
+
+        }
+    }
+}
