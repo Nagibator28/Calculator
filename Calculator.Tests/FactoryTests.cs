4516969edbec9b21c5091233d21973b333eb6355
Nagibator28 <phelixonik@mail.ru>
2015-07-08 15:06:39 +0600

    Test Factory

---
+++ Calculator.Tests/FactoryTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Calculator.TwoArgument;
+using NUnit.Framework;
+
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class FactoryTests
+    {
+        [TestCase(typeof (Addition), "Addition")]
+        [TestCase(typeof (Division), "Division")]
+        [TestCase(typeof (Pow), "Pow")]
+        [TestCase(typeof (Subtraction), "Subtraction")]
+        [TestCase(typeof (Multiplication), "Multiplication")]
+
+        public void FactoryTest(Type type, string name)
+        {
+            Type resultType = TwoArgumentFactory.CreateCalculator(name).GetType();
+            Assert.AreEqual(type, resultType);
+        }
+    }
+}
