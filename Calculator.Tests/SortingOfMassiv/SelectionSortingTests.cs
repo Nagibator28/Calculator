ad2c64dd672390006fd910ab9859b2bea78d9623
Nagibator28 <phelixonik@mail.ru>
2015-07-09 14:21:58 +0600

    Test Selection Sorting

--- Calculator.Tests/SortingOfMassiv/SelectionSortingTests.cs
+++ Calculator.Tests/SortingOfMassiv/SelectionSortingTests.cs
@@ -4,13 +4,13 @@ using NUnit.Framework;
 namespace Calculator.Tests.SortingOfMassiv
 {
     [TestFixture]
-    class BubbleSortingTests
+    class SelectionSortingTests
     {
-        
-        [TestCase(new double [] {12, -3, 0}, new double [] {-3,0,12})]
+
+        [TestCase(new double[] { 12, -3, 0 }, new double[] { -3, 0, 12 })]
         public void Calculate(double[] argument, double[] result)
         {
-            var sort = new BubbleSorting();
+            var sort = new SelectionSorting();
             var testResult = sort.Sort(argument);
             Assert.AreEqual(result, testResult);
         }
